3b10673d07a064adf657eb4f7ac268fd596afaa7
Bernardo de Sousa Garcia <bernardo.sousa.garcia@gmail.com>
2024-10-27 00:29:36 -0300

    Fix - DateTime UTC Pattern

--- renault.risk.manager.Infrastructure/Context/RiskManagerContext.cs
+++ renault.risk.manager.Infrastructure/Context/RiskManagerContext.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using renault.risk.manager.Domain.Entities;
+using renault.risk.manager.Infrastructure.Context.Configurations;
 
 namespace renault.risk.manager.Infrastructure.Context;
 
@@ -16,62 +17,62 @@ public class RiskManagerContext : DbContext
     public DbSet<tb_solution> tb_solutions { get; set; }
     public DbSet<tb_user> tb_users { get; set; }
 
-    // public override int SaveChanges()
-    // {
-    //     // Detecta alterações nas entidades para capturar suas propriedades
-    //     ChangeTracker.DetectChanges();
-    //
-    //     foreach (var entry in ChangeTracker.Entries()
-    //                  .Where(e => e.State == EntityState.Added || e.State == EntityState.Modified))
-    //     {
-    //         foreach (var property in entry.Properties)
-    //         {
-    //             // Verifica se a propriedade é do tipo DateTime
-    //             if (property.Metadata.ClrType == typeof(DateTime) && property.CurrentValue != null)
-    //             {
-    //                 // Converte para UTC caso não esteja definido como UTC
-    //                 var dateTimeValue = (DateTime)property.CurrentValue;
-    //                 if (dateTimeValue.Kind == DateTimeKind.Local || dateTimeValue.Kind == DateTimeKind.Unspecified)
-    //                 {
-    //                     property.CurrentValue = dateTimeValue.ToUniversalTime();
-    //                 }
-    //             }
-    //         }
-    //     }
-    //
-    //     return base.SaveChanges();
-    // }
-    //
-    // public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
-    // {
-    //     ChangeTracker.DetectChanges();
-    //
-    //     foreach (var entry in ChangeTracker.Entries()
-    //                  .Where(e => e.State == EntityState.Added || e.State == EntityState.Modified))
-    //     {
-    //         foreach (var property in entry.Properties)
-    //         {
-    //             if (property.Metadata.ClrType == typeof(DateTime) && property.CurrentValue != null)
-    //             {
-    //                 var dateTimeValue = (DateTime)property.CurrentValue;
-    //                 if (dateTimeValue.Kind == DateTimeKind.Local || dateTimeValue.Kind == DateTimeKind.Unspecified)
-    //                 {
-    //                     property.CurrentValue = dateTimeValue.ToUniversalTime();
-    //                 }
-    //             }
-    //         }
-    //     }
-    //
-    //     return await base.SaveChangesAsync(cancellationToken);
-    // }
+    public override int SaveChanges()
+    {
+        // Detecta alterações nas entidades para capturar suas propriedades
+        ChangeTracker.DetectChanges();
 
-    // protected override void OnModelCreating(ModelBuilder modelBuilder)
-    // {
-    //     base.OnModelCreating(modelBuilder);
-    //
-    //     // modelBuilder.ApplyConfiguration(new tb_projectConfig());
-    //     modelBuilder.ApplyConfiguration(new tb_riskConfig());
-    //     // modelBuilder.ApplyConfiguration(new tb_userConfig());
-    //     // modelBuilder.ApplyConfiguration(new tb_solutionConfig());
-    // }
+        foreach (var entry in ChangeTracker.Entries()
+                     .Where(e => e.State == EntityState.Added || e.State == EntityState.Modified))
+        {
+            foreach (var property in entry.Properties)
+            {
+                // Verifica se a propriedade é do tipo DateTime
+                if (property.Metadata.ClrType == typeof(DateTime) && property.CurrentValue != null)
+                {
+                    // Converte para UTC caso não esteja definido como UTC
+                    var dateTimeValue = (DateTime)property.CurrentValue;
+                    if (dateTimeValue.Kind == DateTimeKind.Local || dateTimeValue.Kind == DateTimeKind.Unspecified)
+                    {
+                        property.CurrentValue = dateTimeValue.ToUniversalTime();
+                    }
+                }
+            }
+        }
+
+        return base.SaveChanges();
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        ChangeTracker.DetectChanges();
+
+        foreach (var entry in ChangeTracker.Entries()
+                     .Where(e => e.State == EntityState.Added || e.State == EntityState.Modified))
+        {
+            foreach (var property in entry.Properties)
+            {
+                if (property.Metadata.ClrType == typeof(DateTime) && property.CurrentValue != null)
+                {
+                    var dateTimeValue = (DateTime)property.CurrentValue;
+                    if (dateTimeValue.Kind == DateTimeKind.Local || dateTimeValue.Kind == DateTimeKind.Unspecified)
+                    {
+                        property.CurrentValue = dateTimeValue.ToUniversalTime();
+                    }
+                }
+            }
+        }
+
+        return await base.SaveChangesAsync(cancellationToken);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // modelBuilder.ApplyConfiguration(new tb_projectConfig());
+        modelBuilder.ApplyConfiguration(new tb_riskConfig());
+        // modelBuilder.ApplyConfiguration(new tb_userConfig());
+        // modelBuilder.ApplyConfiguration(new tb_solutionConfig());
+    }
 }
