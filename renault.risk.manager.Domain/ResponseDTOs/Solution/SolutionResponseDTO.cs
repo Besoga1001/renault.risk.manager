04e7bf517b49385491cccc3c93ce90430693ebdb
Bernardo de Sousa Garcia <bernardo.sousa.garcia@gmail.com>
2024-11-21 19:35:26 -0300

    Fix - Solution Response Fields Name

--- renault.risk.manager.Domain/ResponseDTOs/Solution/SolutionResponseDTO.cs
+++ renault.risk.manager.Domain/ResponseDTOs/Solution/SolutionResponseDTO.cs
@@ -2,18 +2,18 @@ namespace renault.risk.manager.Domain.ResponseDTOs.Solution;
 
 public record SolutionResponseDTO
 (
-    int Id,
-    string Strategy,
-    string ResidualProbability,
-    string ResidualImpact,
-    string ActionValidation,
-    string RiskValidation,
-    DateTime AlertDate,
-    bool Captalization,
-    int UserPilotId,
-    DateTime StartActionPlanDate,
-    string Action,
-    string Observation,
-    DateTime ResolutionDate,
-    int RiskId
+    int SlnId,
+    string SlnStrategy,
+    string SlnResidualProbability,
+    string SlnResidualImpact,
+    string SlnActionValidation,
+    string SlnRiskValidation,
+    DateTime SlnAlertDate,
+    bool SlnCaptalization,
+    int SlnUserPilotId,
+    DateTime SlnStartActionPlanDate,
+    string SlnAction,
+    string SlnObservation,
+    DateTime SlnResolutionDate,
+    int RskId
 );
